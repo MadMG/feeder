b5f2d132fcd8cff7f9fdc55b885636ec56edf63c
MadMG <madmg82@gmail.com>
2016-09-16 16:59:54 +0200

    Use new version of 'netzwerk111-menu'
    + Use Promises for collecting data

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/api/v1', routes);
+app.use('/api/v1/menu', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,57 @@
 var express = require('express');
-var router = express.Router();
 var menu = require('netzwerk111-menu');
+var Pact = require('bluebird');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/menu', function (req, res, next) {
-  menu.retrieve(function (err, data) {
-    if (err) {
-      res.status(err.status || 501);
-      res.json({
-        message: err.message || 'error',
-        error: err
-      });
-    } else {
-      res.json(data);
-    }
-  });
+router.use(function (req, res, next) {
+  req.apiOptions = {
+    prettyPrint: req.query.pretty || false
+  };
+
+  next();
 });
+
+router.get('/', function (req, res) {
+  var prettyPrint = req.apiOptions.prettyPrint;
+
+  retrieveData()
+      .then(function (data) {
+        writeResponse(res, data, prettyPrint);
+      }, function (err) {
+        var responseData = {
+          message: err.message || 'error',
+          error: err
+        };
+
+        writeResponse(res, responseData, prettyPrint, err.status || 501);
+      });
+});
+
+
+function writeResponse (res, responseData, prettyPrint, httpStatus) {
+  if (httpStatus) {
+    res.status(httpStatus);
+  }
+
+  if (prettyPrint) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(responseData, null, 2));
+  } else {
+    res.json(responseData);
+  }
+}
+
+function retrieveData () {
+  return new Pact(function (resolve, reject) {
+    menu.retrieve(function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 
 module.exports = router;
